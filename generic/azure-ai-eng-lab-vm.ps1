eabb12530846895d161b61ad02c21dffc347382a
David Okeyode <asegunlolu@hotmail.com>
2023-07-04 11:08:01 +0100

    Update azure-ai-eng-lab-vm.ps1

--- generic/azure-ai-eng-lab-vm.ps1
+++ generic/azure-ai-eng-lab-vm.ps1
@@ -1,3 +1,4 @@
+$env:chocolateyVersion = '1.4.0'
 Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1'))
 
 choco install git vscode terraform httpie azure-cli az.powershell nodejs.install dotnet-6.0-sdk dotnet-sdk dotnetcore-sdk googlechrome setdefaultbrowser docker-desktop win-no-annoy -y
