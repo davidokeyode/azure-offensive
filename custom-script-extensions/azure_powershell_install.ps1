33fe2f816e28c4aba6b7309ad1d145c47e0c7d5f
David Okeyode <asegunlolu@hotmail.com>
2023-07-05 18:18:53 +0100

    Update azure_powershell_install.ps1

--- custom-script-extensions/azure_powershell_install.ps1
+++ custom-script-extensions/azure_powershell_install.ps1
@@ -1,5 +1,6 @@
-Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
+$env:chocolateyVersion = '1.4.0'
+Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1'))
 
-choco install az.powershell httpie 7zip.install googlechrome setdefaultbrowser win-no-annoy -y
+choco install az.powershell messenger 7zip.install googlechrome setdefaultbrowser win-no-annoy -y
 
 SetDefaultBrowser.exe HKLM "Google Chrome" 
